e788df9a31c76087076af7f6d30cd9b1eba9e691
hoaithuong <hoaithuong251997@gmail.com>
2025-10-27 22:59:59 +0700

    update project

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ASPNET_DK23TTC11_TRANDINHHOAITHUONG_170123537.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
